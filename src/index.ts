86b635b69602ffbb294581536a66bd993b8baf4c
BrindilleDeLaForet <49678641+CorentinDeblock@users.noreply.github.com>
2024-10-04 00:09:15 +0200

    Reapply "Added schema generation feature for frontend"
    This reverts commit 871f6b469394a725cc475c5ae257f5b53167ccdf.

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import UserAPI from './api/userAPI';
 import ArticleAPI from './api/articleAPI';
 import logger from './logger';
 import settings from './settings';
+import AssetsAPI from './api/assetsAPI';
+import OpinionAPI from './api/opinionAPI';
 
 // Create a Prisma client
 const prisma = new PrismaClient();
@@ -18,6 +20,8 @@ app.use(
     new API()
         .addAPI(new UserAPI())
         .addAPI(new ArticleAPI())
+        .addAPI(new AssetsAPI())
+        .addAPI(new OpinionAPI())
         .apply()
 );
 
@@ -31,7 +35,6 @@ app.use('/assets', express.static(path.join(__dirname, '..', 'assets')));
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
-
 
 const server = app.listen(settings.port, () => {
     // Get the address of the server
